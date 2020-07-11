141183e55065050bdd1cc2c73aad6ee80da67c51
Davide Zanetto <davide.zanetto@gmail.com>
2020-07-11 16:54:43 +0900

    added destroy dependencies

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -1,5 +1,5 @@
 class Basket < ApplicationRecord
   belongs_to :user
-  has_many :subscriptions
-  has_many :basket_items
+  has_many :subscriptions, dependent: :destroy
+  has_many :basket_items, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :baskets
-  has_many :subscriptions #as a customer
+  has_many :baskets, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy #as a customer
   has_many :subscriptions_as_farmer, through: :baskets, source: :subscriptions
 end
