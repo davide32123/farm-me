d62417549805ea40db27ed424c71926e98cdf578
Davide Zanetto <davide.zanetto@gmail.com>
2020-07-14 20:51:36 +0900

    basic basket show page

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -1,2 +1,11 @@
 class BasketsController < ApplicationController
+  before_action :find_basket, only: [:show]
+
+  def show; end
+
+  private
+
+  def find_basket
+    @basket = Basket.find(params[:id])
+  end
 end
