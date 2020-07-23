0314b685650aa039c0631be53702ce630fded72b
Liam Baker <liam.baker2310@gmail.com>
2020-07-23 21:45:34 +0900

    fixed variable names

--- app/controllers/farmers_controller.rb
+++ app/controllers/farmers_controller.rb
@@ -2,11 +2,9 @@ class FarmersController < ApplicationController
   def index
     skip_policy_scope
     @baskets = Basket.where(user: current_user)
-    @subscriptions_as_farmer = @baskets.flat_map(&:subscriptions)
-    @subscriptions_as_user = Subscription.where(user: current_user)
+    @subscriptions = @baskets.flat_map(&:subscriptions)
     redirect_to subscriptions_path if @baskets.count.zero?
     authorize @baskets
-    authorize @subscriptions_as_farmer if @subscriptions_as_farmer.nil?
-    authorize @subscriptions_as_user if @subscriptions_as_user.nil?
+    authorize @subscriptions if @subscriptions.nil?
   end
 end
