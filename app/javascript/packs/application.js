9b33f00f9ad8853e178385003887760c3c8d2982
Davide Zanetto <davide.zanetto@gmail.com>
2020-07-25 13:56:44 +0900

    update js delete with hacky solution

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -38,7 +38,7 @@ $(document).on('turbolinks:load', function() {
 
   $('form').on('click', '.remove_record', function(event) {
     $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('tr').hide();
+    $(this).closest('h6').hide();
     return event.preventDefault();
   });
 
