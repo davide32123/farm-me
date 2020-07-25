d2ee079ffb06f6c701aec9b968319cc082548076
Davide Zanetto <davide.zanetto@gmail.com>
2020-07-25 12:29:33 +0900

    testing stuff

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,13 @@ module ApplicationHelper
     end
     link_to(name, '#', class: "add_fields " + args[:class], data: {id: id, fields: fields.gsub("\n", "")})
   end
+
+  def link_to_add_basket_item(name, f, **args)
+    new_object = f.object.basket_items.build
+    id = new_object.id
+    fields = f.fields_for(:basket_items, new_object, child_index: id) do |builder|
+      render 'basket_item', f: builder
+    end
+    link_to(name, '#', class: "add_fields " + args[:class], data: {id: id, fields: fields.gsub("\n", "")})
+  end
 end
